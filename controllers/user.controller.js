48d14e77699cd912156e3d36b3c8619684f985b8
AspiringSaint <wilberttosoydev@outlook.com>
2025-09-12 00:25:38 +0800

    added the user profile view

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,27 @@
+const User = require('../models/User');
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
 
+/**
+ * @description View user info
+ * @route GET /api/users/profile/view
+ * @access Private
+ */
+const getProfileById = asyncHandler(async (req, res) => {
+    const { id } = req.body;
+
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid User Id' });
+    }
+
+    const user = await User.findById(id).select('-password -temporaryPassword');
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+})
 
 module.exports = {
-    
+    getProfileById
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const {
-    
+    getProfileById
 } = require('../controllers/user.controller');
+
+// Profile
+router.get('/profile/view', getProfileById);
+
+
+// Admin CRUD
 
 
 module.exports = router;
